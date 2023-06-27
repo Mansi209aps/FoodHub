efe10f1c9fb34e283882f84e42906eb2a23e1142
Mansi <mansisharma209aps@gmail.com>
2023-06-28 05:01:30 +0530

    footer added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Navbar } from './components/Shared/Navbar';
 import { Home } from './components/Pages/Home';
+import { Footer } from './components/Shared/Footer';
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <Footer />
     </>
   );
 }
--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -111,7 +111,7 @@ export const Home = () => {
                 </div>
             </div>
 
-
+            {/* cards */}
 
             <div className="grid gap-6 row-gap-5 mb-8 lg:grid-cols-4 sm:row-gap-6 sm:grid-cols-2 py-9 px-4">
                 <a href="/" aria-label="View Item">
@@ -203,6 +203,134 @@ export const Home = () => {
                         <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z" />
                     </svg>
                 </a>
+            </div>
+
+
+
+            {/* Variety */}
+            <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+                <div className="grid grid-cols-2 gap-5 row-gap-5 sm:grid-cols-3 lg:grid-cols-6">
+                    <div className="text-center">
+                        <div className="flex items-center justify-center w-10 h-10 mx-auto mb-4 rounded-full bg-indigo-50 sm:w-12 sm:h-12">
+                            <svg
+                                className="w-8 h-8 text-deep-purple-accent-400 sm:w-10 sm:h-10"
+                                stroke="currentColor"
+                                viewBox="0 0 52 52"
+                            >
+                                <polygon
+                                    strokeWidth="3"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    fill="none"
+                                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                                />
+                            </svg>
+                        </div>
+                        <h6 className="mb-2 text-sm font-bold leading-5 tracking-wider uppercase">
+                            World
+                        </h6>
+                    </div>
+                    <div className="text-center">
+                        <div className="flex items-center justify-center w-10 h-10 mx-auto mb-4 rounded-full bg-indigo-50 sm:w-12 sm:h-12">
+                            <svg
+                                className="w-8 h-8 text-deep-purple-accent-400 sm:w-10 sm:h-10"
+                                stroke="currentColor"
+                                viewBox="0 0 52 52"
+                            >
+                                <polygon
+                                    strokeWidth="3"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    fill="none"
+                                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                                />
+                            </svg>
+                        </div>
+                        <h6 className="mb-2 text-sm font-bold leading-5 tracking-wider uppercase">
+                            Sport
+                        </h6>
+                    </div>
+                    <div className="text-center">
+                        <div className="flex items-center justify-center w-10 h-10 mx-auto mb-4 rounded-full bg-indigo-50 sm:w-12 sm:h-12">
+                            <svg
+                                className="w-8 h-8 text-deep-purple-accent-400 sm:w-10 sm:h-10"
+                                stroke="currentColor"
+                                viewBox="0 0 52 52"
+                            >
+                                <polygon
+                                    strokeWidth="3"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    fill="none"
+                                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                                />
+                            </svg>
+                        </div>
+                        <h6 className="mb-2 text-sm font-bold leading-5 tracking-wider uppercase">
+                            Health
+                        </h6>
+                    </div>
+                    <div className="text-center">
+                        <div className="flex items-center justify-center w-10 h-10 mx-auto mb-4 rounded-full bg-indigo-50 sm:w-12 sm:h-12">
+                            <svg
+                                className="w-8 h-8 text-deep-purple-accent-400 sm:w-10 sm:h-10"
+                                stroke="currentColor"
+                                viewBox="0 0 52 52"
+                            >
+                                <polygon
+                                    strokeWidth="3"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    fill="none"
+                                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                                />
+                            </svg>
+                        </div>
+                        <h6 className="mb-2 text-sm font-bold leading-5 tracking-wider uppercase">
+                            Weather
+                        </h6>
+                    </div>
+                    <div className="text-center">
+                        <div className="flex items-center justify-center w-10 h-10 mx-auto mb-4 rounded-full bg-indigo-50 sm:w-12 sm:h-12">
+                            <svg
+                                className="w-8 h-8 text-deep-purple-accent-400 sm:w-10 sm:h-10"
+                                stroke="currentColor"
+                                viewBox="0 0 52 52"
+                            >
+                                <polygon
+                                    strokeWidth="3"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    fill="none"
+                                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                                />
+                            </svg>
+                        </div>
+                        <h6 className="mb-2 text-sm font-bold leading-5 tracking-wider uppercase">
+                            Time
+                        </h6>
+                    </div>
+                    <div className="text-center">
+                        <div className="flex items-center justify-center w-10 h-10 mx-auto mb-4 rounded-full bg-indigo-50 sm:w-12 sm:h-12">
+                            <svg
+                                className="w-8 h-8 text-deep-purple-accent-400 sm:w-10 sm:h-10"
+                                stroke="currentColor"
+                                viewBox="0 0 52 52"
+                            >
+                                <polygon
+                                    strokeWidth="3"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    fill="none"
+                                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                                />
+                            </svg>
+                        </div>
+                        <h6 className="mb-2 text-sm font-bold leading-5 tracking-wider uppercase">
+                            News
+                        </h6>
+                    </div>
+                </div>
             </div>
         </div>
 
