13363d078963805c41dad6b5dca865f6c3e3bbd6
Mansi <mansisharma209aps@gmail.com>
2023-06-29 08:27:00 +0530

    footer color

--- src/components/Shared/Footer.js
+++ src/components/Shared/Footer.js
@@ -3,23 +3,9 @@ import React from "react";
 export const Footer = () => {
     return (
         <div>
-            <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 bg-gray-900">
+            <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 bg-red-600">
                 <div className="grid gap-10 mx-auto sm:grid-cols-2 lg:grid-cols-4 lg:max-w-screen-lg">
-                    <div>
-                        <img
-                            className="object-cover w-24 h-24 rounded-full shadow"
-                            src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
-                            alt="Person"
-                        />
-                        <div className="flex flex-col justify-center mt-2">
-                            <p className="text-lg font-bold">Oliver Aguilerra</p>
-                            <p className="mb-4 text-xs text-gray-800">Product Manager</p>
-                            <p className="text-sm tracking-wide text-gray-800">
-                                Pommy ipsum bent as a nine bob note naff off biscuits nowt, a
-                                cuppa unhand me sir hadn't done it in donkey's years sod's law.
-                            </p>
-                        </div>
-                    </div>
+
                     <div>
                         <img
                             className="object-cover w-24 h-24 rounded-full shadow"
@@ -51,35 +37,20 @@ export const Footer = () => {
                             </p>
                         </div>
                     </div>
-                    <div>
-                        <img
-                            className="object-cover w-24 h-24 rounded-full shadow"
-                            src="https://images.pexels.com/photos/3931603/pexels-photo-3931603.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-                            alt="Person"
-                        />
-                        <div className="flex flex-col justify-center mt-2">
-                            <p className="text-lg font-bold">Martin Garix Potter</p>
-                            <p className="mb-4 text-xs text-gray-800">Good guy</p>
-                            <p className="text-sm tracking-wide text-gray-800">
-                                Est Schlitz shoreditch fashion axe. Messenger bag cupidatat
-                                Williamsburg sustainable aliqua, umami shabby chic artisan duis
-                                pickled.
-                            </p>
-                        </div>
-                    </div>
+
                 </div>
             </div>
 
 
             {/* copyright  */}
-            <div className="flex flex-col justify-between pt-5 pb-10 px-4 bg-gray-900 border-t border-gray-800 sm:flex-row">
-                <p className="text-sm text-gray-500">
+            <div className="flex flex-col justify-between pt-5 pb-10 px-4 bg-red-600 border-t border-white sm:flex-row">
+                <p className="text-sm text-white">
                     © Copyright 2020 Lorem Inc. All rights reserved.
                 </p>
                 <div className="flex items-center mt-4 space-x-4 sm:mt-0">
                     <a
                         href="/"
-                        className="text-gray-500 transition-colors duration-300 hover:text-teal-accent-400"
+                        className="text-white transition-colors duration-300 hover:text-teal-accent-400"
                     >
                         <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
                             <path d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z" />
@@ -87,7 +58,7 @@ export const Footer = () => {
                     </a>
                     <a
                         href="/"
-                        className="text-gray-500 transition-colors duration-300 hover:text-teal-accent-400"
+                        className="text-white transition-colors duration-300 hover:text-teal-accent-400"
                     >
                         <svg viewBox="0 0 30 30" fill="currentColor" className="h-6">
                             <circle cx="15" cy="15" r="4" />
@@ -96,7 +67,7 @@ export const Footer = () => {
                     </a>
                     <a
                         href="/"
-                        className="text-gray-500 transition-colors duration-300 hover:text-teal-accent-400"
+                        className="text-white transition-colors duration-300 hover:text-teal-accent-400"
                     >
                         <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
                             <path d="M22,0H2C0.895,0,0,0.895,0,2v20c0,1.105,0.895,2,2,2h11v-9h-3v-4h3V8.413c0-3.1,1.893-4.788,4.659-4.788 c1.325,0,2.463,0.099,2.795,0.143v3.24l-1.918,0.001c-1.504,0-1.795,0.715-1.795,1.763V11h4.44l-1,4h-3.44v9H22c1.105,0,2-0.895,2-2 V2C24,0.895,23.105,0,22,0z" />
