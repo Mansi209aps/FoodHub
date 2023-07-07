b69347c2ed8165b9613295f819d94c04feff18b4
Mansi <mansisharma209aps@gmail.com>
2023-07-07 17:30:52 +0530

    commom database added

--- Backend/index.js
+++ Backend/index.js
@@ -17,17 +17,16 @@ app.use((req, res, next) => {
     next();
 })
 
-app.use(express.json())
+app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.use('/api', require("./Routes/Auth"))
-
+app.use('/api', require("./Routes/Auth"));
 
 mongoose
     .connect(
-        `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.01byp2c.mongodb.net/foodhubdb?retryWrites=true&w=majority`
+        `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mpyvqgy.mongodb.net/foodhubdb?retryWrites=true&w=majority`
     )
     .then(() => {
         app.listen(5000);
