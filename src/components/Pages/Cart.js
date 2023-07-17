8459154be06292beddbb13ab290cb5dfef7a0e62
Mansi <mansisharma209aps@gmail.com>
2023-07-17 19:31:32 +0530

    cart content

--- src/components/Pages/Cart.js
+++ src/components/Pages/Cart.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useCart } from 'react-use-cart';
+import { FaTrash } from 'react-icons/fa';
 
 const Cart = () => {
     const {
@@ -30,38 +31,49 @@ const Cart = () => {
             <table className='mx-auto'>
                 <thead>
                     <tr className='bg-gray-200'>
-                        <th className='py-2 px-4'>Image</th>
-                        <th className='py-2 px-4'>Title</th>
-                        <th className='py-2 px-4'>Price</th>
-                        <th className='py-2 px-4'>Quantity</th>
-                        <th className='py-2 px-4'></th>
+                        <th className='py-2 px-11 text-center'>Image</th>
+                        <th className='py-2 px-11 text-center'>Title</th>
+                        <th className='py-2 px-11 text-center'>Price</th>
+                        <th className='py-2 px-11 text-center'>Quantity</th>
+                        <th className='py-2 px-11 text-center'>Remove Item</th>
                     </tr>
                 </thead>
                 <tbody>
                     {items.map((item, index) => (
                         <tr key={index} className='border-b'>
-                            <td className='flex items-center py-2 px-4'>
-                                <img src={item.img} alt={item.title} className='h-12' />
-                            </td>
-                            <td className='py-2 px-4'>{item.title}</td>
-                            <td className='py-2 px-4'>{item.price}</td>
                             <td className='py-2 px-4'>
-                                <div className='flex items-center'>
+                                <div className='flex items-center justify-center'>
+                                    <img src={item.img} alt={item.title} className='h-12' />
+                                </div>
+                            </td>
+                            <td className='py-2 px-4 text-center'>{item.title}</td>
+                            <td className='py-2 px-4 text-center'>{item.price}</td>
+                            <td className='py-2 px-4'>
+                                <div className='flex items-center justify-center'>
                                     <button
                                         className='bg-red-600 hover:bg-red-300 text-sm px-2 py-1'
                                         onClick={() => updateItemQuantity(item.id, item.quantity - 1)}
                                     >
                                         -
                                     </button>
-                                    <input
+                                    <div className='flex items-center justify-center border-2 border-gray-500 rounded mx-2 px-4'>
+                                        {item.quantity}
+                                        {/* <input
+                                            type='number'
+                                            value={item.quantity}
+                                            min={1}
+                                            readOnly // Add readOnly attribute
+                                            className='w-12 px-2 py-1 border mx-2 text-center' // Add text-center class
+                                            onChange={(e) => updateItemQuantity(item.id, parseInt(e.target.value, 10))}
+                                        /> */}
+                                    </div>
+                                    {/* <input
                                         type='number'
                                         value={item.quantity}
                                         min={1}
-                                        className='w-16 px-2 py-1 border mx-2'
-                                        onChange={(e) =>
-                                            updateItemQuantity(item.id, parseInt(e.target.value, 10))
-                                        }
-                                    />
+                                        className='w-12 px-2 py-1 border mx-2'
+                                        onChange={(e) => updateItemQuantity(item.id, parseInt(e.target.value, 10))}
+                                    /> */}
                                     <button
                                         className='bg-red-600 hover:bg-red-300 text-sm px-2 py-1'
                                         onClick={() => updateItemQuantity(item.id, item.quantity + 1)}
@@ -71,17 +83,21 @@ const Cart = () => {
                                 </div>
                             </td>
                             <td className='py-2 px-4'>
-                                <button
-                                    className='bg-red-600 hover:bg-red-300 text-sm'
-                                    onClick={() => removeItem(item.id)}
-                                >
-                                    🗑️
-                                </button>
+                                <div className='flex items-center justify-center'>
+                                    <button
+                                        className='bg-red-600 hover:bg-red-300 text-sm'
+                                        onClick={() => removeItem(item.id)}
+                                    >
+                                        <FaTrash />
+                                    </button>
+                                </div>
                             </td>
                         </tr>
                     ))}
                 </tbody>
             </table>
+
+
             <div className='flex items-center justify-between mt-4 mx-5'>
                 <div className='items-center'>
                     <h2>Total Price: {cartTotal}</h2>
